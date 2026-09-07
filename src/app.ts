99eb0ddfd730141f731ec59b8a3ddb88b5c1a528
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-07 08:46:37 +0700

    update code

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,9 @@ const port = 8000;
 
 app.get("/", (req, res) => {
   res.json({
-    
-  })
+    message: "Blog API Berjalan",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
