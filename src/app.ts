457b6d529f056d727c54639bf29a10fbd8712697
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-07 08:58:48 +0700

    update code

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,33 @@ app.get("/categories", async (req, res) => {
   }
 });
 
+app.get("/posts", async (req, res) => {
+    try {
+        const [rows] = await db.query(`
+            SELECT 
+                posts.id,
+                posts.title,
+                posts.content,
+                categories.name AS category
+            FROM posts
+            JOIN categories
+                ON posts.category_id = categories.id
+        `);
+
+        res.status(200).json({
+            success: true,
+            data: rows
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Gagal mengambil data artikel"
+        });
+    }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
