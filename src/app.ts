29ee02b58616e0e4a18031670793864a2a0ced13
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-07 08:46:13 +0700

    update code

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,9 @@ app.use(express.json());
 const port = 8000;
 
 app.get("/", (req, res) => {
-  
+  res.json({
+    
+  })
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
